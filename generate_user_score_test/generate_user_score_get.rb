2923f6da97a7f1933fcc7be525973ad64cd087e0
vishakha-wankhade <vishakha.wankhade@cryptextechnologies.com>
2021-08-24 17:07:49 +0530

    Code committed for test task updated

--- generate_user_score_test/generate_user_score_get.rb
+++ generate_user_score_test/generate_user_score_get.rb
@@ -4,31 +4,36 @@ require 'json'
 require 'byebug'
 
 class  GenerateScore
-	# puts "Enter your github username"
-	# USERINPUT = gets.chomp
-	def get_reponse
-		response = Faraday.get("https://api.github.com/users/ajaydhande/events")
-		data = JSON.parse(response.body)
-		data.each do |d|
-		  @a = calculate(value = 7); puts "IssueEvent" if d['type'] == "IssueEvent"
-		  @b = calculate(value = 6); puts "IssueCommentEvent" if d['type'] == "IssueCommentEvent"
-			@c = calculate(value = 5); puts "PushEvent" if d['type'] == "PushEvent"
-      @d = calculate(value = 4); puts "PullRequestReviewCommentEvent" if d['type'] == "PullRequestReviewCommentEvent"
-			@e = calculate(value = 3); puts "WatchEvent" if d['type'] == "WatchEvent"
-			@f = calculate(value = 2); puts "CreateEvent" if d['type'] == "CreateEvent"
-      @g = calculate(value = 2); puts "PullRequestEvent" if d['type'] == "PullRequestEvent"
-			@h = calculate(value = -1); puts "DeleteEvent" if d['type'] == "DeleteEvent"
-		end
-			result = [@a,@b,@c,@d,@e,@f,@g,@h].inject(&:+)
+	# Method to get Github Event API response
+	def get_reponse(username)
+		response = Faraday.get("https://api.github.com/users/#{username}/events")
+		if response.status == 200
+			data = JSON.parse(response.body)
+		  @a,@b,@c,@d,@e,@f,@g,@h = 0,0,0,0,0,0,0,0
+		  get_events(data)
+			result = [@a,@b,@c,@d,@e,@f,@g,@h].map(&:to_i).sum
 			puts result
+		else
+			puts "Badrequest"
+		end
 	end
-	def calculate(value)
-    return value
+
+  # Method to get events and score from API response
+	def get_events(data)
+		data.each do |d|
+			d['type'] == "IssueEvent" ? (@a += 7; puts "IssueEvent") : @a += 0  
+			d['type'] == "IssueCommentEvent" ? (@b += 6; puts "IssueCommentEvent") : @b += 0 
+			d['type'] == "PushEvent" ? (@c += 5; puts "PushEvent") :  @c += 0
+			d['type'] == "PullRequestReviewCommentEvent" ? (@d += 4; puts "PullRequestReviewCommentEvent") : @d += 0
+			d['type'] == "WatchEvent" ? (@e += 3; puts "WatchEvent") :  @e += 0
+			d['type'] == "CreateEvent" ? (@f += 2; puts "CreateEvent") :  @f += 0
+      d['type'] == "PullRequestEvent" ? (@g += 2; puts "PullRequestEvent") : @g += 0 
+		  d['type'] == "DeleteEvent" ? (@h += -1; puts "DeleteEvent") :  @h += 0
+		end
 	end
 end
 
 generate_score = GenerateScore.new
-generate_score.get_reponse
-
-
-
+puts "Enter your github username : "
+your_user_name = gets.chomp
+generate_score.get_reponse(your_user_name) # to pass dynamic username in API call
